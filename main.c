4774ad75699df6959cd7b44fa5799792731b043d
AndreKaled <andrekaled9@gmail.com>
2025-10-06 13:57:08 -0400

    adiciona teste rápido pra ler e escrever no portal

--- main.c
+++ main.c
@@ -6,6 +6,17 @@
 
 int main(int argc, char** argv){
     Portal* portal = malloc(sizeof(Portal));
-    entradaDados(argc,argv, portal);
+    DadosLidos* dadosLidos = entradaDados(argc,argv, portal);
+
+    /**
+     * FALTA APLICAR THREADS
+     * defini o id como constante 1 só pra testar
+     */
+    for(int i = 0; i < dadosLidos->qtd_alunos; i++){
+        portal_escrever(portal, 1, i, dadosLidos->alunos->nota, dadosLidos->alunos->faltas);
+    }
+    for(int i = 0; i < dadosLidos->qtd_alunos; i++){
+        portal_ler(portal, 1, i);
+    }
     return 0;
 }
